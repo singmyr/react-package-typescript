e36b77e76a31ed9bb63c6bb92c1068dc58bce067
Mattias Singmyr <mattias@singmyr.io>
2019-05-19 22:36:45 +0200

    Adds jest + enzume. Adds an example test for HelloWorld.tsx

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { HelloWorldProps } from '../index';
 
 export default class HelloWorld extends React.Component<HelloWorldProps> {
-  render() {
-    return (
-      <div style={{ color: this.props.color }}>
-        Hello world!
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div style={{ color: this.props.color }}>
+                Hello world!
+            </div>
+        );
+    }
 }
