254805aad75db64ea4686d97c2c4f29909ea7006
Mattias Singmyr <mattias@singmyr.io>
2019-05-19 23:10:48 +0200

    Adds faulty accessibility modifier

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { HelloWorldProps } from '../index';
 
 export default class HelloWorld extends React.Component<HelloWorldProps> {
-    public render() {
+    private render() {
         return <div style={{ color: this.props.color }}>Hello world!</div>;
     }
 }
